62be5d8ea6c5cb6232bdd206b646a76ee5afac00
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-12-16 15:19:27 +0900

    docs: swagger 에 jwt 기능 추가

--- src/main/kotlin/com/yourssu/ssudateserver/config/SwaggerConfig.kt
+++ src/main/kotlin/com/yourssu/ssudateserver/config/SwaggerConfig.kt
@@ -2,11 +2,16 @@ package com.yourssu.ssudateserver.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 
 @Configuration
@@ -14,14 +19,34 @@ class SwaggerConfig {
     @Bean
     fun api(): Docket {
         return Docket(DocumentationType.OAS_30)
+            .securityContexts(listOf(securityContext()))
+            .securitySchemes(listOf(apiKey()))
             .select()
+            .apis(RequestHandlerSelectors.any())
             .apis(RequestHandlerSelectors.basePackage("com.yourssu.ssudateserver"))
             .paths(PathSelectors.any())
             .build()
             .apiInfo(apiInfo())
+            .ignoredParameterTypes(AuthenticationPrincipal::class.java)
     }
 
-    private fun apiInfo(): ApiInfo? {
+    fun securityContext(): SecurityContext {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .build()
+    }
+
+    fun defaultAuth(): List<SecurityReference> {
+        val authorizationScope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopes = arrayOf(authorizationScope)
+        return listOf(SecurityReference("Authorization", authorizationScopes))
+    }
+
+    fun apiKey(): ApiKey {
+        return ApiKey("Authorization", "Bearer", "header")
+    }
+
+    fun apiInfo(): ApiInfo? {
         return ApiInfoBuilder()
             .title("SSUDate")
             .description("Api Documentation")
