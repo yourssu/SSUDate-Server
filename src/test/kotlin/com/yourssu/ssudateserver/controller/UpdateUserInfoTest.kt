948240fd03d1b50c8679652522027444d9f07bf4
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-12-17 15:47:20 +0900

    test: 테스트 변경

--- src/test/kotlin/com/yourssu/ssudateserver/controller/UpdateUserInfoTest.kt
+++ src/test/kotlin/com/yourssu/ssudateserver/controller/UpdateUserInfoTest.kt
@@ -1,9 +1,8 @@
 package com.yourssu.ssudateserver.controller
 
 import com.yourssu.ssudateserver.common.BaseTest
-import com.yourssu.ssudateserver.dto.request.UpdateMaleRequestDto
+import com.yourssu.ssudateserver.dto.request.UpdateRequestDto
 import com.yourssu.ssudateserver.enums.MBTI
-import com.yourssu.ssudateserver.enums.MaleAnimals
 import com.yourssu.ssudateserver.fixture.PrincipalFixture.Companion.setPrincipal
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
@@ -11,21 +10,20 @@ import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.patch
 
 @ActiveProfiles("test")
-class UpdateMaleTest : BaseTest() {
+class UpdateUserInfoTest : BaseTest() {
 
     @Test
     fun updateMaleTest() {
         setPrincipal()
 
-        val requestDto = UpdateMaleRequestDto(
-            animals = MaleAnimals.WOLF,
+        val requestDto = UpdateRequestDto(
             nickName = "NICKNICK",
             mbti = MBTI.INFP,
             introduce = "hihihi",
             contact = "01012345678",
         )
 
-        val test = mockMvc.patch("/users/my/male") {
+        val test = mockMvc.patch("/users/my") {
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(requestDto)
         }
@@ -33,7 +31,7 @@ class UpdateMaleTest : BaseTest() {
         test.andExpect {
             status { isOk() }
             jsonPath("id") { exists() }
-            jsonPath("animals") { value("WOLF") }
+            jsonPath("animals") { value("PUSSUNG") }
             jsonPath("nickName") { value("NICKNICK") }
             jsonPath("mbti") { value("INFP") }
             jsonPath("introduce") { value("hihihi") }
@@ -46,40 +44,15 @@ class UpdateMaleTest : BaseTest() {
     }
 
     @Test
-    fun updateMaleTestFailAnimalsAll() {
-        setPrincipal()
-        val requestDto = UpdateMaleRequestDto(
-            animals = MaleAnimals.ALL,
-            nickName = "NICKNICK",
-            mbti = MBTI.INFP,
-            introduce = "hihihi",
-            contact = "01012345678",
-        )
-        val test = mockMvc.patch("/users/my/male") {
-            contentType = MediaType.APPLICATION_JSON
-            content = objectMapper.writeValueAsString(requestDto)
-        }
-
-        test.andExpect {
-            status { isBadRequest() }
-            jsonPath("message") { value("ALL은 등록불가능 합니다.") }
-        }
-        test.andDo {
-            print()
-        }
-    }
-
-    @Test
     fun updateMaleTestFailNickNameDuplicate() {
         setPrincipal()
-        val requestDto = UpdateMaleRequestDto(
-            animals = MaleAnimals.BEAR,
+        val requestDto = UpdateRequestDto(
             nickName = "testNick2",
             mbti = MBTI.INFP,
             introduce = "hihihi",
             contact = "01012345678",
         )
-        val test = mockMvc.patch("/users/my/male") {
+        val test = mockMvc.patch("/users/my") {
             contentType = MediaType.APPLICATION_JSON
             content = objectMapper.writeValueAsString(requestDto)
         }
